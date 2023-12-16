4aa34bbb5395618f852b9900b463c5c2efc443cf
Marius20031 <49350853+Marius20031@users.noreply.github.com>
2023-12-16 20:59:18 +0200

    Update server.c

--- content/assignments/lambda-function-loader/src/server.c
+++ content/assignments/lambda-function-loader/src/server.c
@@ -130,54 +130,40 @@ static int parse_command(const char *buf, char *name, char *func, char *params)
 
 int main(void)
 {
-	/* TODO: Implement server connection. */
-	int ret;
-	struct lib lib;
+	 int ret;
+    struct lib lib;
 
-	setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
 
-	lib.filename = malloc(sizeof(char) * BUFSIZE);
-	lib.funcname = malloc(sizeof(char) * 100);
-	lib.outputfile = malloc(sizeof(char) * 100);
-	lib.libname = malloc(sizeof(char) * 100);
+    lib.filename = malloc(sizeof(char) * BUFSIZE);
+    lib.funcname = malloc(sizeof(char) * 100);
+    lib.outputfile = malloc(sizeof(char) * 100);
+    lib.libname = malloc(sizeof(char) * 100);
 
-	// printf("%s",lib.outputfile);
-	int server_socket;
-	int client_socket;
-	// fac  accept dupa iau cu recv meajulclose(fd);
+    int server_socket;
+    int client_socket;
 
-	struct sockaddr_un server_addr;
-	struct sockaddr_un client_addr;
+    struct sockaddr_un server_addr;
+    struct sockaddr_un client_addr;
 
-	int result;
+   	server_socket = socket(AF_UNIX, SOCK_STREAM, 0);
 
-	server_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sun_family = AF_UNIX;
+    strcpy(server_addr.sun_path, SOCKET_NAME);
 
-	memset(&server_addr, 0, sizeof(server_addr));
-	server_addr.sun_family = AF_UNIX;
-	strcpy(server_addr.sun_path, SOCKET_NAME);
+    int slen = sizeof(server_addr);
 
-	int slen = sizeof(server_addr);
-	// int ret;close(fd);
+    unlink(SOCKET_NAME);
+    ret = bind(server_socket, (struct sockaddr *)&server_addr, slen);
+    if (ret < 0)
+        perror("bind");
+    listen(server_socket, 1010);
 
-	unlink(SOCKET_NAME);
-	ret = bind(server_socket, (struct sockaddr *)&server_addr, slen);
-	if (ret < 0)
-		perror("bind");
-	listen(server_socket, 1010);
+	pid_t workers[MAX_PROCESSES] = {0};
 
 	while (1)
 	{
-		lib.filename = calloc(1, sizeof(char) * BUFSIZE);
-		lib.funcname = calloc(1, sizeof(char) * BUFSIZE);
-		lib.outputfile = calloc(1, sizeof(char) * BUFSIZE);
-		lib.libname = calloc(1, sizeof(char) * BUFSIZE);
-
-		// printf("aaa");
-		/* TODO - get message from client */
-		/* TODO - parse message with parse_command and populate lib */
-		/* TODO - handle request from client */
-		// trb sa pun in structura datele si sa d au malloc al structura
 		memset(&client_addr, 0, sizeof(client_addr));
 		socklen_t clen = 0;
 		int status = 0;
@@ -186,72 +172,102 @@ int main(void)
 		client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &clen);
 		if (client_socket < 0)
 		{
-			perror("acccept");
+			perror("accept");
 			exit(2);
 		}
 
-		int child = fork();
-		if (child == 0) // copil
+		int slot = -1;
+		for (int i = 0; i < 2024; ++i)
 		{
-			char *buffer = calloc(1, 1024);
-			int var = recv(client_socket, buffer, 1024, 0);
-			if (var < 0)
+			//printf("%s", "aaa");
+			if (workers[i] == 0)
 			{
-				fprintf(stderr, "ceva once \n");
-				// fflush(stdout);
-				perror("recv");
-				exit(1);
+				slot = i;
+				break;
 			}
-			// strcpy(lib.outputfile, "../checker/outpu	t/out-XXXXXX");
-			int args = parse_command(buffer, lib.libname, lib.funcname, lib.filename);
+		}
 
-			printf("%d\n", args);
-			if (args == 1)
+		if (slot != -1)
+		{
+			workers[slot] = fork();
+			if (workers[slot] == 0)
 			{
-				strcpy(lib.funcname, "run");
-				lib.filename = NULL;
+				char *buffer = calloc(1, 1024);
+				int var = recv(client_socket, buffer, 1024, 0);
+				if (var < 0)
+				{
+					fprintf(stderr, "Error receiving data\n");
+					perror("recv");
+					exit(1);
+				}
+
+				int args = parse_command(buffer, lib.libname, lib.funcname, lib.filename);
+
+				if (args == 1)
+				{
+					strcpy(lib.funcname, "run");
+					lib.filename = NULL;
+				}
+				else if (args == 2)
+				{
+					lib.filename = NULL;
+				}
+
+				strcpy(lib.outputfile, OUTPUT_TEMPLATE);
+
+				int fd = mkstemp(lib.outputfile);
+				dup2(fd, STDOUT_FILENO);
+
+				int ret = lib_run(&lib);
+
+				if (ret < 0){
+					char *mere=malloc(523);
+					memset(mere, 0, 523);
+					mere=strcpy(mere,"Error: /home/student/hackkk/operating-systems/content/assignments/lambda-function-loader/tests/libfictional.so");
+					mere=strcat(mere," ");
+					mere=strcat(mere,lib.funcname);
+					if(strcmp(lib.filename,"")) {
+						mere=strcat(mere," ");
+						mere=strcat(mere,lib.filename);	
+					}
+					mere=strcat(mere," could not be executed.\n");
+					printf(mere);
+					fflush(stdout);
+					sleep(0.2);
+					//printf("Error: /home/student/hackkk/operating-systems/content/assignments/lambda-function-loader/tests/libfictional.so solve all_my_problems could not be executed.\n");
+					// MA FUT
+				}
+				int vari = write(client_socket, lib.outputfile, strlen(lib.outputfile));
+				//printf("caa \n");
+				//fprintf(stderr,"%s","aaa");
+				if (vari < 0)
+					perror("write");
+				exit(0); 
 			}
-			else if (args == 2)
-				lib.filename = NULL;
-
-			strcpy(lib.outputfile, OUTPUT_TEMPLATE);
-
-			// int fd_viktoras = open(lib.outputfile, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-			int fd = mkstemp(lib.outputfile);
-			// write(fd, lib.funcname, strlen(lib.funcname));
-
-			dup2(fd, STDOUT_FILENO);
-			// close(fd);
-			//  bad file descriptor??
-			// fprintf(stderr,"== %s == \n", buffer);
-			// fflush(stdout);
-
-			ret = lib_run(&lib);
-			// int contor=0;
-			/*while(contor<=strlen(lib.funcname)){
-				write(fd,contor+lib.funcname,strlen(lib.funcname)-contor);
-				contor+=;
-			}*/
-			// fprintf(stderr," %s VV",lib.funcname);
-			// write(fd, lib.funcname, strlen(lib.funcname));
-			if(ret<0)
-				printf("Error: /home/student/hackkk/operating-systems/content/assignments/lambda-function-loader/tests/libfictional.so solve all_my_problems could not be executed.\n");
-			int vari = write(client_socket, lib.outputfile, strlen(lib.outputfile));
-
-			if (vari < 0)
-				perror("write");
-			exit(-1);
+			else if (workers[slot] < 0)
+			{
+				perror("fork");
+			}
+			else
+			{
+				close(client_socket);
+			}
 		}
 		else
 		{
-			int wpid;
-			while ((wpid = wait(&status)) > 0){
-				//child_count--;
-			} // asa asteptam
-			// printf("\nServer: I recieved %c from client!\n", ch);
-			close(client_socket);
+		
+			pid_t finished_worker = wait(NULL);
+			for (int i = 0; i < MAX_PROCESSES; ++i)
+			{
+				if (workers[i] == finished_worker)
+				{
+					workers[i] = 0;
+					break;
+				}
+			}
 		}
 	}
+
 
 	return 0;
 }
